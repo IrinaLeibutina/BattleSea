25c768de931d95f8b7cc2f5a1cc3b783572310fe
iris0107 <leybutina@mail.ru>
2016-06-03 18:13:32 +0300

    Ready
    Change-Id: Ide58de96eb38c16e585c6728b54ca9107a3cb420

--- src/application/PopularCell.scala
+++ src/application/PopularCell.scala
@@ -49,43 +49,45 @@ class PopularCell {
       second.size
     }
 
-  def findMaxSeq(fieldFilling: Array[Array[Array[Int]]], mas1: Array[Int], replays: Int): Array[Int] = {
-    var sum = 0
-    var amount = 0
+  def findMaxSeq(fieldFilling: Array[Array[Array[Int]]], findSeq: Array[Int], replays: Int): Array[Int] = findSeq match {
+    case _ => {
+      var sum = 0
+      var amount = 0
 
-    for (i <- (0).to(replays - 1)) {
-      var find = i
-      for (l <- (0).to(replays - 1)) {
-        sum = 0
-        for (k <- (20).to(39)) {
-          for (j <- (0).to(9)) {
+      for (i <- (0).to(replays - 1)) {
+        var find = i
+        for (l <- (0).to(replays - 1)) {
+          sum = 0
+          for (k <- (20).to(39)) {
+            for (j <- (0).to(9)) {
 
-            if (fieldFilling(find)(k)(j) == fieldFilling(l)(k)(j)) {
-              sum = sum + 1
-            }
-            if (sum == 200) {
-              amount = amount + 1
+              if (fieldFilling(find)(k)(j) == fieldFilling(l)(k)(j)) {
+                sum = sum + 1
+              }
+              if (sum == 200) {
+                amount = amount + 1
+              }
             }
           }
         }
+        findSeq(i) = amount
+        amount = 0
       }
-      mas1(i) = amount
-      amount = 0
-    }
-    var findMaxRepeat = 0
-    var currentPosition = 0
-    for (i <- (0).to(replays - 1)) {
-      if (mas1(i) > findMaxRepeat) {
-        val change = mas1(i)
-        mas1(i) = findMaxRepeat
-        findMaxRepeat = change
-        currentPosition = i
+      var findMaxRepeat = 0
+      var currentPosition = 0
+      for (i <- (0).to(replays - 1)) {
+        if (findSeq(i) > findMaxRepeat) {
+          val change = findSeq(i)
+          findSeq(i) = findMaxRepeat
+          findMaxRepeat = change
+          currentPosition = i
+        }
       }
-    }
 
-    var result: Array[Int] = new Array[Int](3)
-    result(0) = findMaxRepeat
-    result(1) = currentPosition
-    result
+      var result: Array[Int] = new Array[Int](3)
+      result(0) = findMaxRepeat
+      result(1) = currentPosition
+      result
+    }
   }
 }
